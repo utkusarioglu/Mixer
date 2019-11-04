ddb9c0d82dd3dac7344130cd4145c48773b19faa
Utku Sarioglu <utkusarioglu@hotmail.com>
2019-11-04 21:20:26 +0300

    Tested with M_Namespace and M_State

---
+++ lib-esm/mixer.test.js
@@ -0,0 +1,30 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+import { Parent } from "./mixer";
+test("set_GlobalNamespace", function () {
+    var namespace_input = "App/Random";
+    var cls_sample = (function (_super) {
+        __extends(cls_sample, _super);
+        function cls_sample() {
+            var _this = _super.call(this) || this;
+            _this.set_GlobalNamespace(namespace_input);
+            return _this;
+        }
+        return cls_sample;
+    }(Parent().with()));
+    var cls_instance = new cls_sample();
+    var namespace_return = cls_instance.getGlobalNamespace();
+    expect(namespace_return).toBe(namespace_input);
+});
+//# sourceMappingURL=mixer.test.js.map
--- src/mixer.test.ts
+++ src/mixer.test.ts
@@ -1,11 +1,15 @@
 import { Parent } from "./mixer";
 import { M_Namespace } from "@utkusarioglu/m_namespace";
+import { M_State } from "@utkusarioglu/m_state";
+
 
 test("set_GlobalNamespace", () => {
 
     const namespace_input = "App/Random";
 
-    const cls_sample = class extends Parent().with() {
+    const cls_sample = class extends Parent().with(
+        M_Namespace,
+    ) {
         constructor() {
             super();
             this.set_GlobalNamespace(namespace_input);
@@ -13,7 +17,36 @@ test("set_GlobalNamespace", () => {
     }
 
     const cls_instance = new cls_sample();
-    const namespace_return = cls_instance.getGlobalNamespace();
+    const namespace_return = cls_instance.get_GlobalNamespace();
 
     expect(namespace_return).toBe(namespace_input)
+})
+
+
+test("set_GlobalNamespace", () => {
+
+    const namespace_input = "App/Random";
+    const some_value = "some value";
+
+    const cls_sample = class extends Parent().with(
+        M_Namespace,
+        M_State
+    ) {
+        constructor() {
+            super();
+            this.set_GlobalNamespace(namespace_input);
+            this.initialize_State({
+                SomeProp: some_value
+            })
+        }
+
+        get_SomeProp() {
+            return this.get_State().get_Var("SomeProp");
+        }
+    }
+
+    const cls_instance = new cls_sample();
+    const prop_return = cls_instance.get_SomeProp();
+
+    expect(prop_return).toStrictEqual(some_value);
 })
